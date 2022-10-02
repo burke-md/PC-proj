449df11eaa23c4318647f26aa0630fe41e3f0bf7
MB <michael.dburke56@gmail.com>
2022-10-02 17:23:19 -0500

    Add gaurenteed win logic to computation.

--- pcOutcomes/index.js
+++ pcOutcomes/index.js
@@ -1,28 +1,44 @@
 class Data {
-    constructor(target) {
-        this.rounds = 100_000;
+    constructor(target, pitty) {
+        this.rounds = 1_000_000;
         this.targetPercentage = target;
-        this.outcomePercentage = null;
+        this.pittyRound = pitty;
+
         this.outcomeCounter = 0;
+        this.pittyCounter = 0;
+        this.sequentialLossCounter = 0;
+        this.outcomePercentage = null;
+        this.adjustedOutcomePercentage = null;
     }
 }
 
 const runTest = (info) => {
     for (let i = info.rounds; i > 0; i--) {
         const value = Math.floor(Math.random() * 99);
-        //console.log(value);
+
+        // Count wins, count number of large sequential losses (2nd arg in def)
         if (value < info.targetPercentage - 1) {
             info.outcomeCounter++;
+        } else {
+            info.sequentialLossCounter++;
+            if (info.sequentialLossCounter == info.pittyRound) {
+                info.sequentialLossCounter = 0;
+                info.pittyCounter++;
+            }
         }
     }
 
-    info.outcomePercentage = (info.outcomeCounter / info.rounds) * 100;
+    info.outcomePercentage = info.outcomeCounter / info.rounds * 100;
+    info.adjustedOutcomePercentage = 
+                (info.outcomeCounter + info.pittyCounter) / info.rounds * 100;
 
     console.log(`outcomeCounter: ${info.outcomeCounter}`);
+    console.log(`consecuitive losses before guarenteed win: ${info.pittyRound}`);
     console.log(`targetPercentae: ${info.targetPercentage}`);
     console.log(`outcomePercentae: ${info.outcomePercentage}`);
+    console.log(`adjustedOutcomePercentae: ${info.adjustedOutcomePercentage}`);
 }
 
-const test1 = new Data(10);
+const test1 = new Data(10, 11);
 
 runTest(test1);
